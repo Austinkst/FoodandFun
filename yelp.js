9cd023fb97584cdc8b6984dcb2f9a59a40317bff
giocode <giocordova@gmail.com>
2020-09-26 13:28:15 -0400

    updated the ajax call to retrieve food trucks for specific lat long

--- yelp.js
+++ yelp.js
@@ -2,19 +2,24 @@ $(document).ready(function () {
   
     function yelAPI() {
 
-    var queryURL = 'https://api.yelp.com/v3/businesses/search';    
-    console.log("TEST")
-    
-    // Food Trucks (foodtrucks, [AR, AT, AU, BE, BR, CA, CH, CL, CZ, DE, DK, ES, FI, FR, GB, HK, IE, IT, JP, MX, MY, NL, NO, NZ, PH, PL, PT, SE, TR, TW, US])
+    var yAPI= "ohHuWoT7Lxdl4ivpbDqSxQiXNJRz3l3OdZI3TtuoYQo0df5GNf9pR0rLNcQgyxl-2_fShCwRni0jaM5IlAMB26MYEUYymvu1PWU8XP5snst-bWGSQTsb8dK12UdtX3Yx";
 
+
+
+    var queryURL = 'https://cors-anywhere.herokuapp.com/api.yelp.com/v3/businesses/search?categories=foodtrucks&latitude=37.569796&longitude=-77.473891';    
+    // 37.569796, -77.473891
+    // Food Trucks (foodtrucks, [AR, AT, AU, BE, BR, CA, CH, CL, CZ, DE, DK, ES, FI, FR, GB, HK, IE, IT, JP, MX, MY, NL, NO, NZ, PH, PL, PT, SE, TR, TW, US])
+    // https://api.yelp.com/v3/autocomplete?text=del&latitude=37.786882&longitude=-122.399972
+    
     
     $.ajax({
       url: queryURL,
+      headers: {'Authorization': 'Bearer '+yAPI},
       method: 'GET',
     }).then(function (response) {
       console.log('')
       console.log('    YELP API RESPONSE    ')
-      console.log(respones)
+      console.log(response)
       console.log('')
     });
   };
